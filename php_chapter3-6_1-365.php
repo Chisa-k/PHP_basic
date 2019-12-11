5c21d2b1978fc347bd8f967c86495dd0ecc4b9e4
Chisa <57827121+Chisa-k@users.noreply.github.com>
2019-12-11 23:38:38 +0900

    change

--- php_chapter3-6_1-365.php
+++ php_chapter3-6_1-365.php
@@ -1,5 +1,19 @@
+<pre>
 <?php
 for($i=1; $i<=365; $i++){
-    print($i . \n);
+    print($i . "\n");
+}
+
+$x=1;
+while($x <= 365){
+    print($x . "\n");
+    $x++;
+}
+
+$t=100;
+while($t >= 1){
+    print($t . "\n");
+    $t =$t-2;
 }
 ?>
+</pre>
