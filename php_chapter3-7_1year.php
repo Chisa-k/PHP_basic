a84bae5e706a0d3b543a04306ee8cf246b430012
Chisa <57827121+Chisa-k@users.noreply.github.com>
2019-12-15 19:45:12 +0900

    change

--- php_chapter3-7_1year.php
+++ php_chapter3-7_1year.php
@@ -2,7 +2,7 @@
 
 <?php
 for($i=1; $i<=366; $i++){
-    $day = date('Y/n/j(D)' , strtotime('+' . $i .'day'));
+    $day = date('y/n/j(D)' , strtotime('+' . $i .'day'));
     print($day . "\n");
 }
 ?>
