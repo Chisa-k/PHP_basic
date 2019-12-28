d17d1821e3c106584ac87d4844752e0f76e8d27f
Chisa <57827121+Chisa-k@users.noreply.github.com>
2019-12-28 20:25:42 +0900

    change

--- PHP_basic/php_chapter3-6_1-365.php
+++ PHP_basic/php_chapter3-6_1-365.php
@@ -1,17 +1,17 @@
 <pre>
 <?php
-for($i=1; $i<=365; $i++){
+for ($i=1; $i<=365; $i++) {
     print($i . "\n");
 }
 
 $x=1;
-while($x <= 365){
+while ($x <= 366) {
     print($x . "\n");
     $x++;
 }
 
 $t=100;
-while($t >= 1){
+while ($t >= 1) {
     print($t . "\n");
     $t =$t-2;
 }
